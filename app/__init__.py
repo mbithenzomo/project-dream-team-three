6508afdcffd3499fbaed3a6e20a5ead810bae8b8
Mbithe Nzomo <mbithe.nzomo@andela.com>
2017-01-02 15:46:12 +0300

    Add production env variables

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,10 @@ login_manager = LoginManager()
 def create_app(config_name):
     if os.getenv('FLASK_CONFIG') == "production":
         app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
     else:
         app = Flask(__name__, instance_relative_config=True)
         app.config.from_object(app_config[config_name])
