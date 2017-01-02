e76c1fa568a9377148dc42a9c866d71a896d1c78
Mbithe Nzomo <mbithe.nzomo@andela.com>
2017-01-02 16:07:01 +0300

    Add error page titles

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 # third-party imports
-from flask import abort, Flask, render_template
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -46,14 +46,14 @@ def create_app(config_name):
 
     @app.errorhandler(403)
     def forbidden(error):
-        return render_template('errors/403.html'), 403
+        return render_template('errors/403.html', title='Forbidden'), 403
 
     @app.errorhandler(404)
     def page_not_found(error):
-        return render_template('errors/404.html'), 404
+        return render_template('errors/404.html', title='Page Not Found'), 404
 
     @app.errorhandler(500)
     def internal_server_error(error):
-        return render_template('errors/500.html'), 500
+        return render_template('errors/500.html', title='Server Error'), 500
 
     return app
